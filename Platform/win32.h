4386c717690ddfc810fd6a4e32cf9ae9f4d90660
LoryMaster <lorenzo.stramondo@gmail.com>
2021-12-26 12:12:47 +0100

    Added defines and functions for MessageBoxA/W. Cleaned up a missing space (irrelevant formatting).

--- Platform/win32.h
+++ Platform/win32.h
@@ -2216,7 +2216,7 @@ WS_SYSMENU)
         HMODULE    GetModuleHandleA(LPCTSTR lpModuleName);
         HMODULE    LoadLibraryA(LPCSTR lpLibFileName);
         HANDLE     CreateThread(LPSECURITY_ATTRIBUTES lpThreadAttributes,
-                                SIZE_T dwStackSize,LPTHREAD_START_ROUTINE lpStartAddress,
+                                SIZE_T dwStackSize, LPTHREAD_START_ROUTINE lpStartAddress,
                                 LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId);
         HRESULT    SetThreadDescription(HANDLE hThread, PCWSTR lpThreadDescription);
         
@@ -3480,6 +3480,59 @@ WINSTA_EXITWINDOWS   | WINSTA_ENUMERATE       | WINSTA_READSCREEN)
 #define TCIS_BUTTONPRESSED      0x0001
 #define TCIS_HIGHLIGHTED        0x0002
         
+        
+        /*
+ * MessageBox() Flags
+ */
+        
+#define MB_OK                       0x00000000L
+#define MB_OKCANCEL                 0x00000001L
+#define MB_ABORTRETRYIGNORE         0x00000002L
+#define MB_YESNOCANCEL              0x00000003L
+#define MB_YESNO                    0x00000004L
+#define MB_RETRYCANCEL              0x00000005L
+#define MB_CANCELTRYCONTINUE        0x00000006L
+        
+#define MB_ICONHAND                 0x00000010L
+#define MB_ICONQUESTION             0x00000020L
+#define MB_ICONEXCLAMATION          0x00000030L
+#define MB_ICONASTERISK             0x00000040L
+        
+#define MB_USERICON                 0x00000080L
+#define MB_ICONWARNING              MB_ICONEXCLAMATION
+#define MB_ICONERROR                MB_ICONHAND
+        
+#define MB_ICONINFORMATION          MB_ICONASTERISK
+#define MB_ICONSTOP                 MB_ICONHAND
+        
+#define MB_DEFBUTTON1               0x00000000L
+#define MB_DEFBUTTON2               0x00000100L
+#define MB_DEFBUTTON3               0x00000200L
+#define MB_DEFBUTTON4               0x00000300L
+        
+#define MB_APPLMODAL                0x00000000L
+#define MB_SYSTEMMODAL              0x00001000L
+#define MB_TASKMODAL                0x00002000L
+#define MB_HELP                     0x00004000L // Help Button
+        
+#define MB_NOFOCUS                  0x00008000L
+#define MB_SETFOREGROUND            0x00010000L
+#define MB_DEFAULT_DESKTOP_ONLY     0x00020000L
+        
+#define MB_TOPMOST                  0x00040000L
+#define MB_RIGHT                    0x00080000L
+#define MB_RTLREADING               0x00100000L
+        
+#define MB_SERVICE_NOTIFICATION          0x00200000L
+#define MB_SERVICE_NOTIFICATION_NT3X     0x00040000L
+        
+#define MB_TYPEMASK                 0x0000000FL
+#define MB_ICONMASK                 0x000000F0L
+#define MB_DEFMASK                  0x00000F00L
+#define MB_MODEMASK                 0x00003000L
+#define MB_MISCMASK                 0x0000C000L
+        
+        
         //
         // Windows Menu Interface
         //
@@ -4191,6 +4244,8 @@ WINSTA_EXITWINDOWS   | WINSTA_ENUMERATE       | WINSTA_READSCREEN)
         BOOL     AppendMenuW(HMENU hMenu, UINT uFlags, UINT_PTR uIDNewItem, LPCWSTR lpNewItem);
         BOOL     InsertMenuItemA(HMENU hmenu, UINT item, BOOL fByPosition, LPCMENUITEMINFOA lpmi);
         BOOL     InsertMenuItemW(HMENU hmenu, UINT item, BOOL fByPosition, LPCMENUITEMINFOW lpmi);
+        int      MessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
+        int      MessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType);
         BOOL     SetWindowTextA(HWND hWnd, LPCSTR lpString);
         BOOL     SetWindowTextW(HWND hWnd, LPCWSTR lpString);
         int      GetWindowTextA(HWND hWnd, LPSTR lpString, int nMaxCount);
